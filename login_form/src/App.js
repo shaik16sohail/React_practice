91344b1f8f0173547fc0a0ecea3be56c623f663c
shaik sohail <shaik16sohail@gmail.com>
2024-09-17 12:36:11 +0530

    completed login_form

--- login_form/src/App.js
+++ login_form/src/App.js
@@ -5,7 +5,25 @@ function App() {
   return (
     <div className="App">
       <div className='logo-box'>
-
+        <img src="./twitter.png" alt='twitter_logo' className='logo'></img>
+        <h2>Sign In to Twitter</h2>
+        <button>
+        <img src="./google.jpeg" alt='google_logo'></img>
+        Sign in with Google
+        </button>
+        <button>
+        <img src="./apple.png" alt='apple_logo'></img>
+        Sign in with Apple
+        </button>
+        <hr></hr>
+        <span>Or</span>
+        <form>
+          <input type='text' placeholder='username or emailid'></input>
+          <button>Next</button>
+        </form>
+        {/* <button>forget password</button> */}
+        <p>Don't have an account &nbsp;&nbsp;
+          <a href='#'>Sign up</a></p>
       </div>
     </div>
   );
